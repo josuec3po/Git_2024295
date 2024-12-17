8e1449993da54bbd55da21dd410e21b75d6edff3
Josué Franco Braga <josuefbraga@outlook.com>
2024-12-17 18:05:12 +0000

    Create SimpleOutputCA3.java

---
+++ SimpleOutputCA3.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package simpleoutputca3;
+
+/**
+ *
+ * @author Acer Nitro 5
+ */
+public class SimpleOutputCA3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println(" Josue - 2024295 ");
+        
+    }
+    
+}
